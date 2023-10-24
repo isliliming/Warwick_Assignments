3d925a2212a554251b2e3b7d4077e2acb833f92d
liliming <isliliming@outlook.com>
2023-10-24 08:38:52 +0100

    plot_py

--- lab/lab_week02/01.py
+++ lab/lab_week02/01.py
@@ -5,6 +5,37 @@ Created on Fri Oct 13 21:42:47 2023
 @author: Liming Li
 """
 
-import numpy as np
-print('hello world')
-print('hello again')
+import numpy
+import veusz.embed as veusz
+
+g1 = veusz.Embedded('test01')
+g1.EnableToolbar()
+
+g1.To(g1.Add('page'))
+g1.To(g1.Add('graph'))
+g1.Add('xy', marker='tiehorz', MarkerFill__color='green')
+
+g1.Set('x/autoExtend', False)
+g1.Set('x/autoExtendZero', False)
+
+g1.Zoom(0.8)
+
+# create data and plot
+for i in range(10):
+    x = numpy.arange(0 + i/2., 7. + i/2., 0.05)
+    y = numpy.sin(x)
+    
+    x_data_name = f'x{i}'
+    y_data_name = f'y{i}'
+    
+    g1.SetData(x_data_name, x)
+    g1.SetData(y_data_name, y)
+    
+    g1.Add('xy', xData=x_data_name, yData=y_data_name, marker='tiehorz', MarkerFill__color='green')
+
+    
+
+    
+
+
+
